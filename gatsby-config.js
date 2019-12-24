9588710bc613ae67eb21c54a0c7183eb5c45ec40
ericzakariasson <eric.zakariasson@gmail.com>
2019-12-24 12:01:00 +0100

    Add styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Parkering Göteborg`,
+    description: ``,
+    author: `@ericzakariasson`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,18 +1,30 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
-import { Header } from "./header"
 import "./layout.css"
+import { SEO } from "./seo"
 
-export const Layout: React.FC = ({ children }) => {
-  const data = useStaticQuery(graphql`
+const Wrapper = styled.div`
+  max-width: 60rem;
+  margin: 0 auto;
+  padding: 0 2rem;
+`
+
+const Header = styled.header`
+  padding: 1rem 0;
+`
+
+const SiteTitle = styled.h1`
+  font-size: 1rem;
+`
+
+interface LayoutProps {
+  title: string
+}
+
+export const Layout: React.FC<LayoutProps> = ({ children, title }) => {
+  const { site } = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -23,23 +35,13 @@ export const Layout: React.FC = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <Wrapper>
+      <SEO title={title} />
+      <Header>
+        <SiteTitle>{site.siteMetadata.title}</SiteTitle>
+      </Header>
+      <main>{children}</main>
+      <footer></footer>
+    </Wrapper>
   )
 }
